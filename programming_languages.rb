97554cc9a7cca267e8fe12d4471a8b36ede09736
Scoti Dodson <scotidodson@gmail.com>
2018-09-11 04:12:05 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,34 @@
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+      },
+    :javascript => {
+      :type => "interpreted"
+      },
+    :python => {
+      :type => "interpreted"
+      },
+    :java => {
+      :type => "compiled"
+      }
+    },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+      },
+    :erlang => {
+      :type => "compiled"
+      },
+    :scala => {
+      :type => "compiled"
+      },
+    :javascript => {
+      :type => "interpreted"
+      }
+    }
+}
+
 def reformat_languages(languages)
-  # your code here
+  
 end
