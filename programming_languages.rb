1bc5523b72d6e3dacc3b6b8a4cf79a4f2c102803
Scoti Dodson <scotidodson@gmail.com>
2018-09-11 04:47:33 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -35,7 +35,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, hash|
     hash.each do |language, data|
-      puts hash.keys
+      puts language[:ooo][hash]
       binding.pry
       new_hash[language] = data
       #if languages[:ooo][language] == languages[:functional][language]
