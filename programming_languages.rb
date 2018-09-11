9af8f17a7904a3867261c4b11706853dfc269c48
Scoti Dodson <scotidodson@gmail.com>
2018-09-11 04:51:02 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -35,7 +35,7 @@ def reformat_languages(languages)
   new_hash = {}
   
   languages.each do |style, hash|
-    puts style.keys
+    puts hash.keys
     
       binding.pry
     hash.each do |language, data|
